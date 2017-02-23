f794fc5da2b9e382e7a1732b51c970987dab9302
neo1218 <neo1218@yeah.net>
2017-02-23 21:47:05 +0800

    info login service

--- service/api.py
+++ service/api.py
@@ -1,13 +1,14 @@
 from aiohttp import web
+from .decorator import require_info_login
 
 api = web.Application()
 
 # ====== async view handlers ======
-async def api_index(request):
-    data = {'login': 'api'}
-    return web.json_response(data)
+@require_info_login()
+async def api_index(request, s, sid):
+    return web.json_response({})
 # =================================
 
 # ====== url --------- maps  ======
-api.router.add_route('GET', '/', api_index, name='api_index')
+api.router.add_route('GET', '/info/login/', api_index, name='api_index')
 # =================================
