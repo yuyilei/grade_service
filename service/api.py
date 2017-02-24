b42b0d821746822c11bcbc9c4dd6c7f32e5f4c8a
neo1218 <neo1218@yeah.net>
2017-02-25 01:45:02 +0800

    add grade search

--- service/api.py
+++ service/api.py
@@ -1,4 +1,6 @@
 from aiohttp import web
+from aiohttp.web import Response
+from .spider import get_grade
 from .login_service.service.decorator import require_info_login
 
 api = web.Application()
@@ -6,7 +8,19 @@ api = web.Application()
 # ====== async view handlers ======
 @require_info_login()
 async def grade_all_api(request, s, sid):
-    return web.json_response({})
+    query_string = request.rel_url.query_string
+    if query_string:
+        keys = []; vals = []
+        for _ in query_string.split('&'):
+            keys.append(_.split('=')[0])
+            vals.append(_.split('=')[1])
+        args = dict(zip(keys, vals))
+        xnm = args.get('xnm'); xqm = args.get('xqm')
+        gradeList = await get_grade(s, sid, xnm, xqm)
+        return web.json_response(gradeList)
+    else: # 404: need query string
+        return Response(body = b'{}',
+        content_type = 'application/json', status = 404)
 # =================================
 
 # ====== url --------- maps  ======
--- service/spider.py
+++ service/spider.py
@@ -1,2 +1,44 @@
-import asyncio
+import json
+# import http.cookies.SimpleCookie as SimpleCookie
+from collections import defaultdict
+from http.cookies import SimpleCookie
 import aiohttp
+
+grade_index_url = "http://122.204.187.6/cjcx/cjcx_cxDgXscj.html?doType=query&gnmkdmKey=N305005&sessionUserKey=%s"
+link_index_url = "http://portal.ccnu.edu.cn/roamingAction.do?appId=XK"
+headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"}
+
+async def get_grade(s, sid, xnm, xqm):
+    grade_url = grade_index_url % sid
+    link_url = link_index_url
+    payload = {
+        'xnm': xnm, 'xqm': xqm,
+        '_search': 'false', 'nd': '1487928673156',
+        'queryModel.showCount': 15, 'queryModel.currentPage': 1,
+        'queryModel.sortName': "", 'queryModel.sortOrder': 'asc',
+        'time': 0}
+    s._unsafe = True # enabling cookie processing for IP addresses.
+    async with aiohttp.ClientSession(cookie_jar=s, headers=headers) as session:
+        # error handle
+        async with session.get(link_url) as resp: # 302, Set-Cookie
+            await resp.release() # release response
+            async with session.post(grade_url, data=payload) as resp:
+                try:
+                    # print(await resp.text())
+                    print('Success')
+                    json_data = await resp.json() # may be return None
+                except json.decoder.JSONDecodeError:
+                    print('Failure')
+                    return []
+                gradeList = []
+                _gradeList = json_data.get('items')
+                for _ in _gradeList:
+                    gradeList.append({
+                        'course'  : _.get('kcmc'),
+                        'credit'  : _.get('xf'),
+                        'grade'   : _.get('cj'),
+                        'category': _.get('kclbmc'),
+                        'type'    : _.get('kcgsmc'),
+                        'jxb_id'  : _.get('jxb_id'),
+                        'kcxzmc'  : _.get('kcxzmc')})
+                return gradeList
