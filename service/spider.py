250149a949a4e82df81fd0e32bac92cd278bdc4f
neo1218 <neo1218@yeah.net>
2017-04-13 14:49:44 +0800

    update grade spider

--- service/spider.py
+++ service/spider.py
@@ -1,33 +1,59 @@
 import json
 import aiohttp
-from collections import defaultdict
-from http.cookies import SimpleCookie
 
 grade_index_url = "http://122.204.187.6/cjcx/cjcx_cxDgXscj.html?doType=query&gnmkdmKey=N305005&sessionUserKey=%s"
 link_index_url = "http://portal.ccnu.edu.cn/roamingAction.do?appId=XK"
-headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"}
+login_ticket_url = "http://122.204.187.6/xtgl/login_tickitLogin.html"
+headers = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "Accept-Encoding": "gzip, deflate, sdch",
+    "Accept-Language": "zh-CN,zh;q=0.8,en;q=0.6",
+    "Cache-Control": "no-cache",
+    "Connection": "keep-alive",
+    "Host": "122.204.187.6",
+    "Pragma": "no-cache",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36",
+    "Origin": "http://122.204.187.6",
+    "Referer": "http://122.204.187.6/cjcx/cjcx_cxDgXscj.html"
+}
+
+headers2 = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "Accept-Encoding": "gzip, deflate, sdch",
+    "Accept-Language": "zh-CN,zh;q=0.8,en;q=0.6",
+    "Cache-Control": "no-cache",
+    "Connection": "keep-alive",
+    "Cookie": "JSESSIONID=aaaVXuZqChRsa9iHtIvTv; BIGipServerpool_portal=173058240.20480.0000; UM_distinctid=15b5d8be64f12e-0bf2dfe294ca19-396a7805-13c680-15b5d8be65e45",
+    "Host": "portal.ccnu.edu.cn",
+    "Pragma": "no-cache",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36"
+}
 
 async def _get_grade(session, payload, ip, sid):
     grade_url = grade_index_url % sid
     link_url = link_index_url
-    async with session.get(link_url, proxy=ip, timeout=4):
-        async with session.post(grade_url, data=payload, proxy=ip) as resp:
-            try:
-                json_data = await resp.json()
-                gradeList = []
-                _gradeList = json_data.get('items')
-                for _ in _gradeList:
-                    gradeList.append({
-                        'course'  : _.get('kcmc'),
-                        'credit'  : _.get('xf'),
-                        'grade'   : _.get('cj'),
-                        'category': _.get('kclbmc'),
-                        'type'    : _.get('kcgsmc'),
-                        'jxb_id'  : _.get('jxb_id'),
-                        'kcxzmc'  : _.get('kcxzmc')})
-                return gradeList
-            except json.decoder.JSONDecodeError:
-                return [None]
+    async with session.get(link_url, timeout=4, headers=headers2, proxy=ip):
+        async with session.get(login_ticket_url, headers=headers, proxy=ip):
+            async with session.post(grade_url, data=payload, headers=headers) as resp:
+                try:
+                    json_data = await resp.json()
+                    gradeList = []
+                    _gradeList = json_data.get('items')
+                    for _ in _gradeList:
+                        gradeList.append({
+                            'course'  : _.get('kcmc'),
+                            'credit'  : _.get('xf'),
+                            'grade'   : _.get('cj'),
+                            'category': _.get('kclbmc'),
+                            'type'    : _.get('kcgsmc'),
+                            'jxb_id'  : _.get('jxb_id'),
+                            'kcxzmc'  : _.get('kcxzmc')
+                        })
+                    return gradeList
+                except json.decoder.JSONDecodeError:
+                    return [None]
 
 async def get_grade(s, sid, ip, xnm, xqm):
     payload = {
@@ -36,10 +62,11 @@ async def get_grade(s, sid, ip, xnm, xqm):
         'queryModel.showCount': 15, 'queryModel.currentPage': 1,
         'queryModel.sortName': "", 'queryModel.sortOrder': 'asc',
         'time': 0}
-    s._unsafe = True # enabling cookie processing for IP addresses.
-    async with aiohttp.ClientSession(cookie_jar=s, headers=headers) as session:
+    async with aiohttp.ClientSession(cookie_jar=aiohttp.CookieJar(unsafe=True),
+            cookies=s, headers=headers) as session:
         try:
             rv = await _get_grade(session, payload, ip, sid)
             return rv
-        except Exception:
+        except Exception as e:
+            print(e)
             return [None] # faild...retry
