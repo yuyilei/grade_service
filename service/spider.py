9500ad451d230ffc6b529c4258b2ad5be082770c
neo1218 <neo1218@yeah.net>
2017-04-23 09:28:06 +0800

    move new info login spider

--- service/spider.py
+++ service/spider.py
@@ -31,29 +31,31 @@ headers2 = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36"
 }
 
-async def _get_grade(session, payload, ip, sid):
-    grade_url = grade_index_url % sid
-    link_url = link_index_url
-    async with session.get(link_url, timeout=4, headers=headers2, proxy=ip):
-        async with session.get(login_ticket_url, headers=headers, proxy=ip):
-            async with session.post(grade_url, data=payload, headers=headers) as resp:
-                try:
-                    json_data = await resp.json()
-                    gradeList = []
-                    _gradeList = json_data.get('items')
-                    for _ in _gradeList:
-                        gradeList.append({
-                            'course'  : _.get('kcmc'),
-                            'credit'  : _.get('xf'),
-                            'grade'   : _.get('cj'),
-                            'category': _.get('kclbmc'),
-                            'type'    : _.get('kcgsmc'),
-                            'jxb_id'  : _.get('jxb_id'),
-                            'kcxzmc'  : _.get('kcxzmc')
-                        })
-                    return gradeList
-                except json.decoder.JSONDecodeError:
-                    return [None]
+# async def _get_grade(session, payload, ip, sid):
+#     grade_url = grade_index_url % sid
+#     link_url = link_index_url
+#     async with session.get(link_url, timeout=4, headers=headers2, proxy=ip):
+#         async with session.get(login_ticket_url, headers=headers, proxy=ip):
+#             async with session.post(grade_url, data=payload, headers=headers) as resp:
+#                 try:
+#                     json_data = await resp.json()
+#                     gradeList = []
+#                     _gradeList = json_data.get('items')
+#                     for _ in _gradeList:
+#                         gradeList.append({
+#                             'course'  : _.get('kcmc'),
+#                             'credit'  : _.get('xf'),
+#                             'grade'   : _.get('cj'),
+#                             'category': _.get('kclbmc'),
+#                             'type'    : _.get('kcgsmc'),
+#                             'jxb_id'  : _.get('jxb_id'),
+#                             'kcxzmc'  : _.get('kcxzmc')
+#                         })
+#                     print('got!')
+#                     return gradeList
+#                 except json.decoder.JSONDecodeError as e:
+#                     print(e)
+#                     return [None]
 
 async def get_grade(s, sid, ip, xnm, xqm):
     payload = {
@@ -61,12 +63,30 @@ async def get_grade(s, sid, ip, xnm, xqm):
         '_search': 'false', 'nd': '1487928673156',
         'queryModel.showCount': 15, 'queryModel.currentPage': 1,
         'queryModel.sortName': "", 'queryModel.sortOrder': 'asc',
-        'time': 0}
+        'time': 0
+    }
+    grade_url = grade_index_url % sid
     async with aiohttp.ClientSession(cookie_jar=aiohttp.CookieJar(unsafe=True),
             cookies=s, headers=headers) as session:
-        try:
-            rv = await _get_grade(session, payload, ip, sid)
-            return rv
-        except Exception as e:
-            print(e)
-            return [None] # faild...retry
+        # rv = await _get_grade(session, payload, ip, sid)
+        async with session.post(grade_url, data=payload) as resp:
+            try:
+                json_data = await resp.json()
+                gradeList = []
+                _gradeList = json_data.get('items')
+                for _ in _gradeList:
+                    gradeList.append({
+                        'course'  : _.get('kcmc'),
+                        'credit'  : _.get('xf'),
+                        'grade'   : _.get('cj'),
+                        'category': _.get('kclbmc'),
+                        'type'    : _.get('kcgsmc'),
+                        'jxb_id'  : _.get('jxb_id'),
+                        'kcxzmc'  : _.get('kcxzmc')
+                    })
+                print('got!')
+                return gradeList
+            except json.decoder.JSONDecodeError as e:
+                print(e)
+                return [None]
+            # return rv
