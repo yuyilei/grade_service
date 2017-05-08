12df790703599699caca870c5c841025c38ea709
neo1218 <neo1218@yeah.net>
2017-05-09 00:17:24 +0800

    tests pass

--- tests/test_grade_api.py
+++ tests/test_grade_api.py
@@ -8,21 +8,22 @@ def test_grade_api(app):
         with TestClient(app, loop=loop) as client:
             
             async def get_headers():
-                auth_header = {'Authorization': 'Basic %s' % base64.b64encode(b'2014210761:2014210761')}
+                nonlocal client
+                auth_header = {'Authorization': 'Basic %s' % base64.b64encode(b'2014210761:2014210761').decode()}
                 async with aiohttp.ClientSession(headers=auth_header) as session:
                     async with session.get('http://info_login_service:8080/api/info/login/') as resp:
                         assert resp.status == 200
                         json_data = await resp.json()
-                        cookie_header = auth_header.update(json_data.get('cookie'))
-                        sookie_header['Sid'] = json_data.get('sid')
+                        auth_header.update(json_data.get('cookie'))
+                        cookie_header = auth_header
+                        cookie_header['Sid'] = json_data.get('sid')
                         return cookie_header
 
             async def _test_grade_get_api():
                 nonlocal client
                 cookie_header = await get_headers()
-                resp = await client.get('/api/grade/search/?xnm=2015&xqm=3', headers=cookie_header)
+                resp = await client.get('/api/grade/?xnm=2014&xqm=3', headers=cookie_header)
                 assert resp.status == 200
                 print(".... grade get api [OK]")
 
             loop.run_until_complete(_test_grade_get_api())
-            loop.close()
